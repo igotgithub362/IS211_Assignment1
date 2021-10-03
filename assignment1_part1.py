d216320c7658c696bbaccb83464cc0b35dcde3ad
igotgithub362 <70487035+igotgithub362@users.noreply.github.com>
2021-10-02 22:16:32 -0800

    Update assignment1_part1.py

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -24,6 +24,8 @@ def testListDivide():
             raise ListDivideException
         if not listDivide([]) == 0:
             raise ListDivideException
+        if not listDivide([1,2,3,4,5],1) == 5:
+            raise ListDivideException
         print("All test cases passed")
     except ListDivideException:
         print("Division exception occured")
